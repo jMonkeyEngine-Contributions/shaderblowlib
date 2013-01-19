4a12d39638282bd446ff341acb314c7c728ee55b
paulgeraskin@gmail.com <paulgeraskin@gmail.com@6be73205-4d7c-4720-a439-38de59cd8c6f>
2013-01-19 07:42:33 +0000

    [ShaderBlow] electricity, addede another example!

---
+++ ShaderBlow/test-src/com/shaderblow/test/electricity/Electricity5Demo.java
@@ -0,0 +1,90 @@
+package com.shaderblow.test.electricity;
+
+import com.jme3.animation.AnimChannel;
+import com.jme3.animation.AnimControl;
+import com.jme3.animation.LoopMode;
+import com.jme3.app.SimpleApplication;
+import com.jme3.asset.plugins.FileLocator;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
+import com.jme3.material.Material;
+import com.jme3.material.RenderState.BlendMode;
+import com.jme3.material.RenderState.FaceCullMode;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.queue.RenderQueue.Bucket;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.shape.Box;
+import com.jme3.texture.Texture;
+
+/**
+ * @author cvlad
+ */
+public class Electricity5Demo extends SimpleApplication {
+
+    public static void main(String[] args) {
+        Electricity5Demo app = new Electricity5Demo();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        
+        this.assetManager.registerLocator("assets", FileLocator.class);          
+        
+        Spatial man = assetManager.loadModel("TestModels/LightBlow/jme_lightblow.mesh.xml");
+        Material matMan = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
+        man.setMaterial(matMan);
+        
+        Electricity5Material mat = new Electricity5Material(assetManager, "ShaderBlow/MatDefs/Electricity/Electricity5.j3md");
+        mat.setLayers(10);
+        mat.setWidth(0.2f);
+        mat.setFloat("speed", 0.1f);
+        mat.setFloat("thickness", 0.14f);
+        Texture noiseTex = assetManager.loadTexture("TestTextures/Electricity/noise.png");
+        noiseTex.setWrap(Texture.WrapMode.Repeat);
+        mat.setTexture("noise", noiseTex);
+        mat.getAdditionalRenderState().setBlendMode(BlendMode.AlphaAdditive);
+        mat.getAdditionalRenderState().setDepthWrite(false);
+        mat.getAdditionalRenderState().setDepthTest(true);
+        mat.getAdditionalRenderState().setFaceCullMode(FaceCullMode.Off);
+        mat.setColor("color", ColorRGBA.Blue);
+        
+        for (Spatial child : ((Node)man).getChildren()){
+            if (child instanceof Geometry){
+                Geometry electricity = new Geometry("electrified_" + child.getName());
+                electricity.setQueueBucket(Bucket.Transparent);
+                electricity.setMesh(((Geometry)child).getMesh());
+                electricity.setMaterial(mat);
+                ((Node)man).attachChild(electricity);
+            }
+        }
+        
+
+        rootNode.attachChild(man);
+        DirectionalLight light = new DirectionalLight();
+        light.setColor(new ColorRGBA(0.6f,0.6f,0.6f,0.6f));
+        AmbientLight ambient = new AmbientLight();
+        ambient.setColor(new ColorRGBA(0.6f,0.6f,0.6f,0.6f));
+        light.setDirection(new Vector3f(-1,-1,-1));
+        rootNode.addLight(light);
+        rootNode.addLight(ambient);
+        
+        ((Node)man).setLocalTranslation(new Vector3f(0,-1.5f,5));
+        
+        getFlyByCamera().setMoveSpeed(getFlyByCamera().getMoveSpeed()*10);
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        //TODO: add update code
+    }
+
+    @Override
+    public void simpleRender(RenderManager rm) {
+        //TODO: add render code
+    }
+}
---
+++ ShaderBlow/test-src/com/shaderblow/test/electricity/Electricity5Material.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shaderblow.test.electricity;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.material.Material;
+import com.jme3.material.MaterialDef;
+import com.jme3.renderer.RenderManager;
+import com.jme3.scene.Geometry;
+
+/**
+ *
+ * @author cvlad
+ */
+public class Electricity5Material extends Material {
+    
+    int layers = 10;
+    float width = 1;
+    
+    public Electricity5Material(MaterialDef def){
+        super(def);
+        this.layers = layers;
+    }
+    
+    public Electricity5Material(AssetManager contentMan, String defName){
+        super(contentMan, defName);
+        this.layers = layers;
+    }
+    
+    public void setLayers(int layers){
+        this.layers = layers;
+    }
+    
+    public int getLayers(){
+        return this.layers;   
+    }
+    
+    public void setWidth(float width){
+        this.width = width;
+    }
+    
+    public float getWidth(){
+        return this.width;
+    }
+    
+    public void render(Geometry geometry, RenderManager manager){
+        // TODO: copy&paste code from LodControl to get lod working, since geometry.getLodLevel() has a maximum based on the mesh
+        for (int i = 1; i <= layers; i++){   
+            this.setFloat("width", width*i/layers);
+            this.setFloat("layer", ((float)i)/layers);
+            super.render(geometry, manager);
+        }
+    }
+}
